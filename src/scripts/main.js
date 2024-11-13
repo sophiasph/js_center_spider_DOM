8ca35348ec70149620681bd1d6b15e3eb14d8c0b
Sophia <sophia13gal@gmail.com>
2024-11-13 14:12:49 +0200

    add corected solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@ const wallRect = wall.getBoundingClientRect();
 const spiderRect = spider.getBoundingClientRect();
 
 const centreX = (wallRect.width - spiderRect.width) / 2;
-const centreY = (wallRect.width - spiderRect.width) / 2;
+const centreY = (wallRect.height - spiderRect.height) / 2;
 
 spider.style.position = 'absolute';
 spider.style.left = `${centreX}px`;
