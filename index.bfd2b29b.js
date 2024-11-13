ce2db77178764bb2f316aee8b40d22de043ccbc4
Sophia <sophia13gal@gmail.com>
2024-11-13 14:10:49 +0200

    make build

---
+++ index.bfd2b29b.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall").getBoundingClientRect(),o=t.getBoundingClientRect(),i=(e.width-o.width)/2,l=(e.width-o.width)/2;t.style.position="absolute",t.style.left="".concat(i,"px"),t.style.top="".concat(l,"px");
+//# sourceMappingURL=index.bfd2b29b.js.map
